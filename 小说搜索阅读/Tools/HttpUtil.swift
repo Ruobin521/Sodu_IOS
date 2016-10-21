8eb5d11306cbedae447f949a82f9c4302ef016e3
ruobin <83250112@qq.com>
2016-10-21 18:26:40 +0800

    研究afnet

--- 小说搜索阅读/Tools/HttpUtil.swift
+++ 小说搜索阅读/Tools/HttpUtil.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import AFNetworking
+
 
 enum RequestMethod {
     
@@ -16,10 +18,35 @@ enum RequestMethod {
 }
 
 
-class  HttpUtil {
+class  HttpUtil :AFHTTPSessionManager  {
     
-    static let  instance = HttpUtil()
-     
+    
+    static var  instance:HttpUtil =  {
+        
+        let shared = HttpUtil()
+        
+   //[manage.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+       // [urlRequest addValue:@"gzip" forHTTPHeaderField:@"Accept-Encoding"];
+
+      
+        
+        //shared.requestSerializer.setValue("gzip", forHTTPHeaderField: "Content-Encoding")
+      
+        shared.requestSerializer.setValue("gzip, deflate, sdch", forHTTPHeaderField: "Accept-Encoding")
+        shared.requestSerializer.timeoutInterval = 10
+        
+        
+
+        shared.responseSerializer = AFHTTPResponseSerializer()
+        shared.responseSerializer.acceptableContentTypes?.insert("text/html")
+        shared.responseSerializer.acceptableContentTypes?.insert("application/x-gzip")
+        
+        
+        return shared
+        
+    }()
+    
+    
     
     func request(url:String ,requestMethod: RequestMethod, postStr: String?, timeOut:TimeInterval = 15.0, completion: @escaping (_ result:String?, _ isSuccess :Bool) ->() )  {
         
@@ -47,14 +74,52 @@ class  HttpUtil {
             
             guard let _ = data , let  html = String(data: data!, encoding: .utf8) else {
                 
-                  completion(nil,false)
-                  return
+                completion(nil,false)
+                return
             }
             
             completion(html,true)
             
             }.resume()
         
+    }
+    
+    
+    func AFrequest(url:String ,requestMethod: RequestMethod, postStr: String?,parameters: [String: AnyObject]?, timeOut:TimeInterval = 15.0, completion: @escaping (_ result:Any?, _ isSuccess :Bool) ->() )  {
+        
+        
+       
+        
+        let success  = { (task: URLSessionDataTask? , json: Any?) -> () in
+            
+            completion(json , true)
+            print(json)
+            
+            var str = String(data:(json as? Data)!, encoding: String.Encoding)
+            
+            print(str)
+            
+            
+        }
+        
+        
+        let failure  = { (task: URLSessionDataTask?, error: Error?) -> () in
+            
+            print(task?.response)
+            
+            completion(nil, false)
+            print(error)
+            
+        }
+        
+        if requestMethod == .GET {
+            
+            get(url, parameters: parameters, progress: nil, success: success, failure: failure)
+            
+        } else {
+            
+            post(url, parameters: parameters, progress: nil, success: success, failure: failure)
+        }
     }
     
 }
